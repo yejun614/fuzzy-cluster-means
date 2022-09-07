8d04e14f52f4ca1055b8f0115b119a9080c48da4
YeJun, Jung <yejun614@naver.com>
2022-09-07 16:50:49 +0900

    Add console invisible in windows

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // Hide console window on Windows in release
+
 use eframe;
 use egui;
 use egui::plot::{Plot, PlotPoints, Points};
